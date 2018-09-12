4d4b00efc6dbae344ddadabdf2451b12178a1afb
lining01 <lining01@xiaoshouyi.com>
2018-09-12 09:47:32 +0800

    hellow world controller and reload on change

---
+++ src/main/java/com/hhg/jerry/controller/HelloController.java
@@ -0,0 +1,21 @@
+package com.hhg.jerry.controller;
+
+import com.alibaba.fastjson.JSONObject;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by lining on 2018/9/11.
+ */
+@RestController
+public class HelloController {
+    @RequestMapping("/hello")
+    public String index(){
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("name","dennis");
+        jsonObject.put("age", 33);
+        jsonObject.put("happy", false);
+        jsonObject.put("skill",new String[]{"c#","java"});
+        return jsonObject.toJSONString();
+    }
+}
---
+++ src/test/java/com/hhg/jerry/controller/HelloControllerTest.java
@@ -0,0 +1,28 @@
+package com.hhg.jerry.controller;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+/**
+ * Created by lining on 2018/9/11.
+ */
+public class HelloControllerTest {
+    private MockMvc mvc;
+    @Before
+    public void setUp() throws Exception {
+        mvc = MockMvcBuilders.standaloneSetup(new HelloController()).build();
+    }
+    @Test
+    public void getHello() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/hello").accept(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andDo(MockMvcResultHandlers.print())
+                .andReturn();
+    }
+}
